8899a5fbe6a5e932949d20307e7505e0f8a98a2d
Marek Matulka <marek@matulka.net>
2015-11-03 13:35:18 +0000

    Fix errors

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ echo "Remove unwanted applications"
 sudo dnf remove claws-mail *abrt* xfburn -y
 
 echo "Install required software"
-sudo dnf install -y evolution brasero docky
+sudo dnf install -y evolution banshee docky
 
 echo "Install RPMFusion repository..."
 sudo dnf install -y --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 echo "Install CDR Tools repository"
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-cdrtools.repo
-sudo -y install cdrecord mkisofs cdda2wav
+sudo dnf -y install cdrecord mkisofs cdda2wav brasero
