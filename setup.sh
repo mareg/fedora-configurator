4d9ab6147de3a7b3d4791121101eaffa821470c4
Marek Matulka <marek@matulka.net>
2015-11-03 13:37:33 +0000

    Add (some) dev tools

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ sudo dnf install -y evolution banshee docky
 echo "Install RPMFusion repository..."
 sudo dnf install -y --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo "Install CDR Tools repository"
+echo "Install CDRecord tools repository"
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-cdrtools.repo
-sudo dnf -y install cdrecord mkisofs cdda2wav brasero
+sudo dnf install -y cdrecord mkisofs cdda2wav brasero
+
+echo "Install dev tools"
+sudo dnf install eclipse-platform meld ghex
